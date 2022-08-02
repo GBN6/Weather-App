ec20ea4c6e8f5cf8257f9a4ad315c514acbcea06
GBN6 <czesiox8@gmail.com>
2022-08-02 15:56:36 +0200

    Create render weather info function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import {
   getForecast,
   requestCityUrl,
   getCoord,
+  weatherDate,
+  weatherTime,
 } from './weatherApi';
 
 const cityUrl = requestCityUrl('Warsaw');
-getCoord(cityUrl);
+
+async function test() {
+  let coords = await getCoord(cityUrl);
+  let forcecastUrl = requestForecastUrl(coords, 'metric');
+  let forecastWeather = await getForecast(forcecastUrl);
+  console.log(forecastWeather);
+  console.log(weatherDate(forecastWeather.current.dt));
+  console.log(weatherTime(forecastWeather.current.dt));
+
+}
+test();
