8d48a370129cbbe689ed6f72204839f0f2635466
GBN6 <czesiox8@gmail.com>
2022-08-03 15:25:40 +0200

    Add initial load function

--- src/index.js
+++ src/index.js
@@ -6,26 +6,58 @@ import {
   weatherDate,
   weatherTime,
 } from './weatherApi';
-import { renderWeatherData, renderWeatherDetails, renderDailyWeatherForecast } from './dom';
+import {
+  renderWeatherData,
+  renderWeatherDetails,
+  renderDailyWeatherForecast,
+} from './dom';
 
 const searchButton = document.querySelector('.btn');
 const form = document.querySelector('.form');
 
-searchButton.addEventListener('click', test);
+searchButton.addEventListener('click', () => test(units));
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-async function test() {
-  const userInput = document.querySelector('.user-input');
-  let cityUrl = requestCityUrl(userInput.value);
-  let coords = await getCoord(cityUrl);
-  let forcecastUrl = requestForecastUrl(coords, 'metric');
-  let forecastWeather = await getForecast(forcecastUrl);
-  forecastWeather.name = coords.name;
-  forecastWeather.country = coords.country;
-  renderWeatherData(forecastWeather, 'metric');
-  renderWeatherDetails(forecastWeather, 'metric');
-  renderDailyWeatherForecast(forecastWeather, 'metric');
-  userInput.value = '';
+let units = 'metric';
+let degreesToggle = false;
+let lastCity = 'warsaw';
+
+async function test(unit, firstLoad = false) {
+  try {
+    let city;
+
+    if (firstLoad) {
+      city = 'warsaw';
+    } else {
+      const userInput = document.querySelector('.user-input');
+      city = userInput.value;
+    }
+
+    if (!city) return;
+
+    if (degreesToggle) {
+      city = lastCity;
+    }
+
+    lastCity = city;
+    console.log(city);
+
+    let cityUrl = requestCityUrl(city);
+    let coords = await getCoord(cityUrl);
+    let forcecastUrl = requestForecastUrl(coords, unit);
+    let forecastWeather = await getForecast(forcecastUrl);
+    forecastWeather.name = coords.name;
+    forecastWeather.country = coords.country;
+    renderWeatherData(forecastWeather, unit);
+    renderWeatherDetails(forecastWeather, unit);
+    renderDailyWeatherForecast(forecastWeather, unit);
+    degreesToggle = false;
+  } catch (error) {
+    document.querySelector('.error-message').style.visibility = 'visible';
+  }
+  document.querySelector('.user-input').value = '';
 }
+
+test(units, true);
